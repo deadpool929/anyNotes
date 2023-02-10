575634b33b6daaf792154edfa9b67854920c858b
deadpool929 <74432497+deadpool929@users.noreply.github.com>
2023-02-10 18:23:37 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 const { MONGOURI } = require("./config/keys");
-const mongoURI = "";
+const mongoURI = MONGOURI;
 const connectToMongo = async () => {
   mongoose.connect(mongoURI, () => {
     console.log("Connected to Database Successfully");
